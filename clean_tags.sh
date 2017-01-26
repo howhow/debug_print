ea917d67c4716d460fe7e9532f21e50f897fdd3d
How Chen <how.chen@gemalto.com>
2017-01-26 17:22:28 +0800

    remote local scripts

--- clean_tags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "clean ALL tags"
-#rm cscope.files
-#rm cscope.out
-rm -f ./cscope.*
-rm -f ./tags
-echo "tags clean done!!"
--- post_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-make
-make test
-for x in ./**/*.c; do
-    (cd "${x%/*}" && gcov "${x##/}")
-done
