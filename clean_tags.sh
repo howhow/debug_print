5128356f539fe190845c601cb6b83d671590fcaa
How Chen <how.chen@gemalto.com>
2018-09-19 11:01:41 +0800

    remove useless file 1. build/clean tag shell script

--- clean_tags.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "clean ALL tags"
-rm *cscope.*out*
-rm tags
-echo "tags clean done!!"
--- post_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-make $1 && make test && make testcov && make testcovr && make clean
-
